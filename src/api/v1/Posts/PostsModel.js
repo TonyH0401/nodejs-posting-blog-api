feaa6711e21dcc168947a3c9a2e368e5c5ce8c4a
Lê Gia Phú <phugiale2912@gmail.com>
2024-03-25 13:50:06 +0700

    init Posts

---
+++ src/api/v1/Posts/PostsModel.js
@@ -0,0 +1,6 @@
+const Schema = require("mongoose").Schema;
+// Import Database Connection:
+const { mongodbConn } = require("../../../database/mongoose");
+// Define AccountsModel:
+// Exports:
+module.exports = {};
